a0f699ce5f6f1c8dfd284b5ccbeeb859ad5138b9
Phil <philip-lisak@lambdastudents.com>
2020-09-06 13:01:40 -0700

    Created components folder and UserCard.js. Initial setup for state variables

---
+++ react-user-card/src/components/UserCard.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Card, Icon, Image } from 'semantic-ui-react';
+
+class UserCard extends React.Component {
+    state = {
+        username: '',
+        followers: ''
+    }
+
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default UserCard
